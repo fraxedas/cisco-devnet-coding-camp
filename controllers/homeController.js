7045f0fdf22f03dfbb4648b59b959d63d5552687
Oscar Fraxedas <fraxedas@gmail.com>
2016-05-14 15:31:43 -0400

    refactoring

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,35 +1,8 @@
 (function (homeController) {
 	homeController.init = function (app) {
 
-		var Sparky = require('node-sparky');
-
-		var sparky = new Sparky({ token: 'NDEyZmJlOWQtZWI1MS00YzUwLWI5ZmQtYjRhMmRkZDYxNzliNjAwOGVkMjUtYzc0' });
-
 		app.get("/", function (req, res) {
 			res.send({ name: "Oscar & Shayak" });
-		});
-
-		app.get("/rooms", function (req, res) {
-			sparky.rooms.get(function (err, results) {
-				res.send(results);
-			});
-		});
-
-		app.all("/rooms/:roomId", function (req, res) {
-			var roomId = req.params.roomId;
-			sparky.messages.get(roomId, 10, function (err, results) {
-				res.send({ error: err, result: results });
-			});
-		});
-
-		app.all("/rooms/:roomId/:text", function (req, res) {
-			var roomId = req.params.roomId;
-			var text = req.params.text;
-			sparky.message.send.room(roomId, {
-				text: text
-			}, function (err, results) {
-				res.send({ error: err, result: results });
-			});
 		});
 	};
 })(module.exports);
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 (function(controllers) {
     var homeController = require('./homeController');
     var tropoController = require('./tropoController.js');
+    var sparkController = require('./sparkController.js');
     
     controllers.init = function (app){
         homeController.init(app);
         tropoController.init(app);
+        sparkController.init(app);
     };
 })(module.exports);
