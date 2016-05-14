ddc2bf6420e0c0c85ecd14d1188763472b99958e
Oscar Fraxedas <fraxedas@gmail.com>
2016-05-14 12:42:21 -0400

    spark

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,18 @@
 (function (homeController) {
 	homeController.init = function (app) {
 
+		var Sparky = require('node-sparky');
+
+		var sparky = new Sparky({ token: 'NDEyZmJlOWQtZWI1MS00YzUwLWI5ZmQtYjRhMmRkZDYxNzliNjAwOGVkMjUtYzc0' });
+
 		app.get("/", function (req, res) {
-			res.send({ name: "Oscar & Shayak"});
+			res.send({ name: "Oscar & Shayak" });
+		});
+
+		app.get("/rooms", function (req, res) {
+			sparky.rooms.get(function (err, results) {
+				res.send(results);
+			});
 		});
 	};
 })(module.exports);
